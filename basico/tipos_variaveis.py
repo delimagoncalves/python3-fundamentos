ad964808563adcd9f58e746c69f42de5f2f5b0a6
Gilson lima <>
2022-09-13 15:15:41 -0300

    Tipos de variáveis em python

---
+++ basico/tipos_variaveis.py
@@ -0,0 +1,16 @@
+# Variável do tipo String
+letras = "abcdef"
+
+# Variável do tipo int
+numero = 4
+
+# Variável do tipo float
+decimal = 2.12
+
+# Variável booleana
+maior_de_idade = True
+
+print("String: ",letras)
+print("inteiro: ",numero)
+print("decimal: ",decimal)
+print("Booleana: ",maior_de_idade)
