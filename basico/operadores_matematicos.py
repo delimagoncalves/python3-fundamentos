59fc6b759b7b3f2696540514374427e4f0de85d4
Gilson lima <>
2022-09-13 15:59:39 -0300

    Operações matemáticas em Python

---
+++ basico/operadores_matematicos.py
@@ -0,0 +1,8 @@
+print("OPERADOR\tOPERAÇÃO\tEXEMPLO\t\tRESULTADO")
+print("**\t\tExponenciação\t2**3\t\t",2**3)
+print("%\t\tMódulo/Resto\t22%8\t\t",22%8)
+print("//\t\tDivisão Ineira\t22//8\t\t",22//8)
+print("/\t\tDivisão\t\t22/8\t\t",22/8)
+print("*\t\tMultiplicação\t2*3\t\t",2*3)
+print("-\t\tSubtração\t5-3\t\t",5-3)
+print("+\t\tAdição\t\t2+3\t\t",2+3)
