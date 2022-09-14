98d97d47e117419fb26e1a05c65ba6681dee9cec
Gilson lima <>
2022-09-14 11:51:12 -0300

    Opeadores de comparacao

---
+++ basico/operadores_comparacao.py
@@ -0,0 +1,7 @@
+print("OPERADOR\tSIGNIFICADO")
+print("==\t\tIgual a")
+print("!=\t\tDiferente de")
+print("<\t\tMenor que")
+print(">\t\tMaior que")
+print("<=\t\tMenor ou igual a")
+print(">=\t\tMaior ou igual a")
