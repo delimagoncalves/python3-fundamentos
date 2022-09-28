770ec220ef3039591a42b6745b202c34baddbe41
Gilson Lima <>
2022-09-28 07:52:33 -0300

    laços de repetição em python

---
+++ basico/lacos_repeticao.py
@@ -0,0 +1,16 @@
+#Laços de repetição(while..)
+soma = 0
+laco = 0
+while(soma<100):
+    laco+=1
+    soma+=10
+
+print("Quantidade de iterações: ",laco)
+print("Valor total da soma: ",soma)
+
+#Laço com for..in..range
+valor = 100
+for n in range(1,11):
+    valor-=n
+
+print("Valor total da subtração: ",valor)
